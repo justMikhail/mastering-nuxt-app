1f376e7b7c98ee7451020f6595177d4fc75b475d
justMikhail <followupmikhail@gmail.com>
2023-02-12 01:25:30 +0300

    Creating the useFetchWithCache Composable

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,13 +1,8 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
+import { LessonWithPath } from '~/types/course';
 
-  const { data, error } = await useFetch(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`);
-
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
-  }
-
-  return data;
-};
+export default async (
+  chapterSlug: string,
+  lessonSlug: string
+) => useFetchWithCache<LessonWithPath>(
+  `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+);
